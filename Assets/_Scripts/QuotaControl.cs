4930fd2628b44e687c7889956e86c5da50ff9f7e
hastrong <148916556+hastrong@users.noreply.github.com>
2025-02-05 21:49:35 -0800

    UI elements
    I made UI for the score and quota. Color and design can be changed later to fit the theme if we want. I also hooked up the UI so it counts the players score and tracks which quota it needs to display.

---
+++ Assets/_Scripts/QuotaControl.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuotaControl : MonoBehaviour
+{
+    public float levelOneQuota = 1000f;
+    public float levelTwoQuota = 2000f;
+    public float levelThreeQuota = 3000f;
+}
---
+++ Assets/_Scripts/UI_Manager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Manager : MonoBehaviour
+{
+    public TMP_Text quotaText;
+    public TMP_Text scoreText;
+    public PlayerController playerController;
+    public QuotaControl quotaControl;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + playerController.totalScore.ToString();
+        quotaText.text = "Round 1 Quota: " + quotaControl.levelOneQuota.ToString();
+        //above will need work later (hooking it up to work and changing the code accordingly)
+        //I might make this process more eficcient later (for the quota), but it works for now.
+    }
+}
