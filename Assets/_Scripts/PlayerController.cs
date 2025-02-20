878cb0cb42a05e5a4bb39ae935a443eb5ac4097e
hastrong <148916556+hastrong@users.noreply.github.com>
2025-02-20 00:55:58 -0800

    cache UI
    UI is present and code is set up for it to change depending on the player's score

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float totalScore = 0f;
+    public float totalCache = 0f;
 }
--- Assets/_Scripts/UI_Manager.cs
+++ Assets/_Scripts/UI_Manager.cs
@@ -7,6 +7,7 @@ public class UI_Manager : MonoBehaviour
 {
     public TMP_Text quotaText;
     public TMP_Text scoreText;
+    public TMP_Text cacheText;
     public PlayerController playerController;
     public QuotaControl quotaControl;
 
@@ -15,6 +16,7 @@ public class UI_Manager : MonoBehaviour
     {
         scoreText.text = "Score: " + playerController.totalScore.ToString();
         quotaText.text = "Round 1 Quota: " + quotaControl.levelOneQuota.ToString();
+        cacheText.text = "Cache: " + playerController.totalCache.ToString();
         //above will need work later (hooking it up to work and changing the code accordingly)
         //I might make this process more eficcient later (for the quota), but it works for now.
     }
