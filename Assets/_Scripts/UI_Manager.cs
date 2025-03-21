2db93f29a58c9eecf187282af6b2878b3e87b197
vyenda <angeleenarellano@gmail.com>
2025-03-21 15:17:31 -0700

    UI Script for Pocket set up

--- Assets/_Scripts/UI_Manager.cs
+++ Assets/_Scripts/UI_Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements; 
 using TMPro;
 
 public class UI_Manager : MonoBehaviour
@@ -19,5 +20,12 @@ public class UI_Manager : MonoBehaviour
         cacheText.text = "Cache: " + playerController.totalCache.ToString();
         //above will need work later (hooking it up to work and changing the code accordingly)
         //I might make this process more eficcient later (for the quota), but it works for now.
+    }
+
+    //Not sure if this has to be in OnEnable, I was following a tutorial to help me learn this and they used their example in this.
+    private void OnEnable()
+    {
+        //This is a root reference to the UI, giving us access to it's elements.
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
     }
 }
