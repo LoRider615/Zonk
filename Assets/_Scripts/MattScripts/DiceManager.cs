fbac2209bb84850f959033c4e07b18855cab2c4b
koocing <mattgai02@gmail.com>
2025-03-22 18:26:29 -0700

    added determined dice roll system

---
+++ Assets/_Scripts/MattScripts/DiceManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceManager : MonoBehaviour
+{
+    public DiceRoller[] diceArray;
+
+    // this will have to be changed, as each dice will have it's own desired value
+    public void RollAllDice()
+    {
+        foreach (DiceRoller die in diceArray)
+        {
+            int number = 3;
+            die.RollDice(number);
+        }
+    }
+}
